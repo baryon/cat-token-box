3c3058375b5895e54c9bc1ec552171246e4ffb3d
Long Li <lilong@gmail.com>
2024-09-11 17:17:58 +0900

    no message

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,7 +5,7 @@ git switch main
 
 sudo chmod 777 -R docker/data
 sudo chmod 777 -R docker/pgdata
-sudo chmod 777 -R docker/logs
+sudo chmod 777 -R docker/log
 
 # cp .env.production .env
 docker compose -f docker-compose.yml build
